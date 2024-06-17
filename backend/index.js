85a7fa1011f01a0ca18f0879aa6858ed0d8a4856
bschack <benharris1102@gmail.com>
2024-06-17 01:34:29 -0400

    chore: Update CORS configuration to allow credentials and remove unnecessary code

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const port = 8080;
 
 app.use(helmet());
-app.use(cors({ credentials: true }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 // CSRF protection 
