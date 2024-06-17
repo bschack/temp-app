d034ec763593d4957ce874f7b97f695d6bf54317
bschack <benharris1102@gmail.com>
2024-06-16 20:38:30 -0400

    chore: Remove unnecessary files and code related to ESLint, Next.js, and configuration

--- weather-app/src/helpers/GetWeatherPrediction.ts
+++
@@ -1,7 +0,0 @@
-export default async function GetWeather(endpoint: string | undefined) {
-  if (!endpoint) {
-    throw new Error("No weather endpoint provided.");
-  }
-  const data = await fetch(endpoint);
-  return data;
-}
